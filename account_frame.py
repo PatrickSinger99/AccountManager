4df072914222a77d09d03018d6d838ea7844bb61
Patrick Singer <60189276+PatrickSinger99@users.noreply.github.com>
2024-02-20 14:13:48 +0100

    update

--- account_frame.py
+++ account_frame.py
@@ -2,13 +2,18 @@ import tkinter as tk
 from PIL import ImageTk, Image
 from tkinter.font import Font
 from tooltip import create_tool_tip
+import subprocess
 
 
 class AccountFrame(tk.Frame):
 
     default_account_logo_path = "./icons/default_logo.png"
-    unknowm_attribute_logo_path = "./icons/unknown_attribute.png"
-    password_logo_path = "./icons/password.png"
+
+    colors = {
+        "detail_default": "black",
+        "detail_hover": "grey",
+        "detail_click": "white"
+    }
 
     def __init__(self, controller, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -16,7 +21,6 @@ class AccountFrame(tk.Frame):
         self.configure()
 
         self.account_logo = ImageTk.PhotoImage(Image.open(AccountFrame.default_account_logo_path))
-        self.unknowm_attribute_logo = ImageTk.PhotoImage(Image.open(AccountFrame.unknowm_attribute_logo_path))
 
         """UPPER LEVEL FRAMES"""
 
@@ -26,8 +30,8 @@ class AccountFrame(tk.Frame):
         self.info_frame = tk.Frame(self, bg=self.cget("bg"))
         self.info_frame.pack(side="left", fill="both", expand=True, pady=5)
 
-        self.expand_frame = tk.Frame(self, bg="light grey", width=50)
-        self.expand_frame.pack(side="left", fill="y", padx=5, pady=5)
+        self.options_frame = tk.Frame(self, bg="light grey", width=50)
+        self.options_frame.pack(side="left", fill="y", padx=5, pady=5)
 
         """IMAGE FRAME"""
 
@@ -43,29 +47,45 @@ class AccountFrame(tk.Frame):
         self.data_frame = tk.Frame(self.info_frame, bg=self.info_frame.cget("bg"))
         self.data_frame.pack(fill="x")
 
-        self.add_attribute("email", "examplemail@lol.com")
-        self.add_attribute("password", "asdp12apsdja2")
-        self.add_attribute("name", "test name")
+        self.add_detail("email", "examplemail@lol.com")
+        self.add_detail("password", "asdp12apsdja2")
+        self.add_detail("name", "test name")
 
-
-    def add_attribute(self, attribute, value):
+    def add_detail(self, detail, value):
         frame_bg = self.data_frame.cget("bg")
-        new_attribute_frame = tk.Frame(self.data_frame, bg=frame_bg)
+        new_detail_frame = tk.Frame(self.data_frame, bg=frame_bg)
 
-        if attribute in self.controller.attribute_imgs.keys():
-            attribute_logo = self.controller.attribute_imgs[attribute]
+        if detail in self.controller.acc_detail_imgs.keys():
+            detail_logo = self.controller.acc_detail_imgs[detail]
         else:
-            attribute_logo = self.unknowm_attribute_logo
+            detail_logo = self.controller.acc_detail_imgs["unknown_detail"]
 
-        attribute_logo = tk.Label(new_attribute_frame, image=attribute_logo, bg=frame_bg)
-        attribute_logo.pack(side="left")
-        create_tool_tip(attribute_logo, attribute)
+        detail_logo = tk.Label(new_detail_frame, image=detail_logo, bg=frame_bg)
+        detail_logo.pack(side="left")
+        create_tool_tip(detail_logo, detail.capitalize())
 
-        attribute_value = tk.Label(new_attribute_frame, text=value, bg=frame_bg, font=Font(size=10))
-        attribute_value.pack(side="left")
+        detail_value = tk.Label(new_detail_frame, text=value, bg=frame_bg, font=Font(size=10), cursor="hand2",
+                                fg=AccountFrame.colors["detail_default"],
+                                activeforeground=AccountFrame.colors["detail_click"])
+        detail_value.bind("<Button-1>", lambda e: self.copy_to_clipboard(value))
+        detail_value.bind("<Enter>", lambda e: self.on_detail_enter(detail_value))
+        detail_value.bind("<Leave>", lambda e: self.on_detail_leave(detail_value))
+        detail_value.pack(side="left")
 
-        copy_button = tk.Button(new_attribute_frame, text="COPY", command=lambda: print(value), bd=0)
-        copy_button.pack(side="left")
+        new_detail_frame.pack(fill="x")
 
-        new_attribute_frame.pack(fill="x")
+    @staticmethod
+    def on_detail_enter(detail_obj):
+        detail_obj.configure(fg=AccountFrame.colors["detail_hover"])
 
+    @staticmethod
+    def on_detail_leave(detail_obj):
+        detail_obj.configure(fg=AccountFrame.colors["detail_default"])
+
+    @staticmethod
+    def copy_to_clipboard(value):
+        # Copy text to clipboard
+        cmd = 'echo ' + str(value).strip() + '|clip'
+        subprocess.check_call(cmd, shell=True)
+
+        print(f"Copied \"{value}\" to clipboard")
--- gui.py
+++ gui.py
@@ -5,24 +5,85 @@ from PIL import ImageTk, Image
 
 class AccountManager(tk.Tk):
 
-    attribute_logo_paths = {
+    acc_details_logo_paths = {
         "password": "./icons/password.png",
-        "email": "./icons/email.png"
+        "email": "./icons/email.png",
+        "unknown_detail": "./icons/unknown_detail.png"
     }
 
     def __init__(self):
         super().__init__()
 
+        """WINDOW ATTRIBUTES"""
+
         self.title("Account Manager")
         self.configure(bg="white")
 
-        self.attribute_imgs = {}
-        for attribute, img_path in AccountManager.attribute_logo_paths.items():
-            self.attribute_imgs[attribute] = ImageTk.PhotoImage(Image.open(img_path))
+        """IMAGES"""
 
+        # Load every account detail icon as a Tk Image into a dictionary
+        self.acc_detail_imgs = {}
+        for attribute, img_path in AccountManager.acc_details_logo_paths.items():
+            self.acc_detail_imgs[attribute] = ImageTk.PhotoImage(Image.open(img_path))
 
-        self.test_frame = AccountFrame(self, bg="#ace3d4")
-        self.test_frame.pack(padx=5, pady=5, fill="x")
+        """BODY"""
+
+        # FRAME:
+        self.body_frame = tk.Frame(self, width=300, height=500)
+        self.body_frame.pack_propagate(False)
+        self.body_frame.pack(side="bottom")
+
+        # Create canvas as wrapper for scroll widgets
+        self.scrollable_canvas = tk.Canvas(self.body_frame, highlightthickness=0, relief='ridge', bg="red")
+
+        # Frame inside canvas
+        self.content_frame = tk.Frame(self.scrollable_canvas)
+        self.content_frame.pack(side="left")
+        self.content_frame.bind("<Configure>", lambda e: self.scrollable_canvas.configure(
+            scrollregion=self.scrollable_canvas.bbox("all")))
+
+        # Scrollbar for canvas
+        self.content_scrollbar = tk.Scrollbar(self.scrollable_canvas, orient="vertical",
+                                              command=self.scrollable_canvas.yview)
+        self.content_scrollbar.pack(side="right", fill="y")
+
+        # Add a window with the content frame inside canvas. Link scrollbar to canvas
+        self.canvas_window = self.scrollable_canvas.create_window((0, 0), window=self.content_frame, anchor="nw")
+        self.scrollable_canvas.configure(yscrollcommand=self.content_scrollbar.set)
+        self.scrollable_canvas.pack(side="bottom", fill="both", expand=True)
+
+        # Set window width inside canvas to canvas width
+        self.scrollable_canvas.bind("<Configure>", self.set_canvas_window_width)
+
+        # Bind scrollwheel to canvas to enable scrolling with mouse
+        self.content_frame.bind("<Enter>", lambda x: self.bind_canvas_to_mousewheel(self.scrollable_canvas))
+        self.content_frame.bind("<Leave>", lambda x: self.unbind_canvas_from_mousewheel(self.scrollable_canvas))
+
+        # TEMP
+        for _ in range(10):
+            test_frame = AccountFrame(master=self.content_frame, controller=self, bg="#ace3d4")
+            test_frame.pack(padx=5, pady=5, fill="x")
+
+        """NOTIFICATION BAR"""
+        self.update()  # Update base tk window before to get current height for frame placement
+        self.notification_frame = tk.Frame(self.body_frame, height=30, bg="green")
+        self.notification_frame.place(x=0, y=self.body_frame.winfo_height()-30, relwidth=1)
+
+    def set_canvas_window_width(self, event):
+        new_width = event.width - 17  # 17 = width of scrollbar
+        self.scrollable_canvas.itemconfig(self.canvas_window, width=new_width)
+
+    def bind_canvas_to_mousewheel(self, canvas):
+        # Bind mousewheel only if more elements are available than fit the screen
+        if self.content_frame.winfo_height() > self.scrollable_canvas.winfo_height():
+            canvas.bind_all("<MouseWheel>", self.on_mousewheel)
+
+    @staticmethod
+    def unbind_canvas_from_mousewheel(canvas):
+        canvas.unbind_all("<MouseWheel>")
+
+    def on_mousewheel(self, event):
+        self.scrollable_canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
 
 
 if __name__ == '__main__':
